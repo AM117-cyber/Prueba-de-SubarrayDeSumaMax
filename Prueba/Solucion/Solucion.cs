59a4bff324efa7212c51cf5a26b4e82173535c0f
AM117-cyber <106854772+AM117-cyber@users.noreply.github.com>
2022-06-07 03:33:57 -0700

    Add files via upload

---
+++ Prueba/Solucion/Solucion.cs
@@ -0,0 +1,37 @@
+﻿namespace Weboo.Examen
+{
+    public static class Solucion
+    {//metodo que da respuesta final.
+    public static int[] SubarrayDeSumaMaxima(int[] numeros)
+    {int[] mayorsuma = SumaMax(numeros);
+    int inicio = mayorsuma[0];
+    int final = mayorsuma[1];
+    int[] subarrayDeSumaMaxima = new int[final-inicio+1];
+     for (int m = 0; m <= final-inicio; m++)
+   {
+   subarrayDeSumaMaxima[m]= numeros[inicio+m];
+   }
+    return subarrayDeSumaMaxima;
+    }
+        public static int[] SumaMax(int[] a)
+{   int inicio = 0;
+    int final = 0;
+    int maxSuma = a[0];
+    for (int i = 0; i < a.Length; i++)
+    { int sumaActual = 0;
+        for (int j = i; j < a.Length; j++)
+        {   sumaActual += a[j];
+            if(sumaActual>=maxSuma)
+            {
+               maxSuma = sumaActual;
+               inicio = i;
+               final = j;
+            }
+            }
+        }
+    int[] mayorsuma = {inicio,final};
+    return mayorsuma;
+}
+    }
+}
+    
---
+++ Prueba/Tester/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Weboo.Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Case1();
+        }
+
+        static void Case1()
+        {
+            Console.WriteLine("Case 1");
+            int[] numeros = new int[] {-7,-6,-5};
+            int[] solucionEsperada = new int[] { 2, 7 };
+            Test(numeros, solucionEsperada);
+        }
+
+        static void Test(int[] numeros, int[] solucionEsperada)
+        {
+            int[] solucionObtenida = Solucion.SubarrayDeSumaMaxima(numeros);
+            if (solucionEsperada.SequenceEqual(solucionObtenida))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("OK");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR");
+                Console.WriteLine($"Se esperaba: {{ {string.Join(", ", solucionEsperada)} }}");
+                Console.WriteLine($"Se obtuvo: {{ {string.Join(", ", solucionObtenida)} }}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
